e78e78cef952bafbf865314c9f2dc80257e9b6d5
hcwtam <hcwtam@gmail.com>
2022-01-02 21:41:15 +0100

    init project, basic ui

---
+++ renderer.js
@@ -0,0 +1,73 @@
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
+const canvas = document.getElementById("progress-bar");
+const context = canvas.getContext("2d");
+const button = document.getElementById("button");
+const panel = document.getElementById("panel");
+const items = document.getElementById("items");
+const size = document.getElementById("size");
+const timeLeft = document.getElementById("time-left");
+
+// date states
+let startDateValue, endDateValue;
+
+startInput.addEventListener("change", (event) => {
+  startDateValue = event.target.value;
+  if (!endDateValue) endInput.setAttribute("min", startDateValue);
+  if (endDateValue && new Date(endDateValue) < new Date(startDateValue)) {
+    endInput.value = "";
+    endInput.setAttribute("min", startDateValue);
+    endDateValue = "";
+  }
+});
+
+endInput.addEventListener("change", (event) => {
+  endDateValue = event.target.value;
+});
+
+// progress bar
+let al = 0;
+let start = 4.72;
+let cw = context.canvas.width / 2;
+let ch = context.canvas.height / 2;
+let diff;
+
+function progressBar() {
+  diff = (al / 100) * Math.PI * 2;
+  context.clearRect(0, 0, 400, 400);
+  context.beginPath();
+  context.arc(cw, ch, 160, 0, 2 * Math.PI, false);
+  context.fillStyle = "#FFF";
+  context.fill();
+  context.strokeStyle = "#eee";
+  context.stroke();
+  context.fillStyle = "#000";
+  context.strokeStyle = "#84c6c6";
+  context.textAlign = "center";
+  context.lineWidth = 5;
+  context.font = "8pt Verdana";
+  context.beginPath();
+  context.arc(cw, ch, 160, start, diff + start, false);
+  context.stroke();
+  if (al >= 100) {
+    clearTimeout(bar);
+  }
+
+  al += 0.1;
+}
+
+let bar = setInterval(progressBar, 5);
+
+// panel
+let showPanel = false;
+if (!showPanel) {
+  panel.style.display = "none";
+}
+
+button.onclick = () => {
+  showPanel = true;
+  button.style.display = "none";
+  panel.style.display = "flex";
+
+}
+
